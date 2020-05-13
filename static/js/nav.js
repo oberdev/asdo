72f4cd28b10df0ea935613454d514cc1442b72b8
oberdev <qovalski@gmail.com>
2020-05-13 04:39:39 +0300

    remove staticfiles

--- static/js/nav.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.navbar-gray').removeClass('active').addClass('active');
-
-    $('.navbar-white .navbar-left a').on('click', function(){
-       $('.navbar-white .navbar-left li').removeClass('active');
-       $(this).parent().addClass('active');
-   });
-});
